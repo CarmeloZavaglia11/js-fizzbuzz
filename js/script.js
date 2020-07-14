423039e570f34f7c2fde58a3ab02b82d57da744d
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-07-14 13:42:08 +0200

    Checkpoint n.3

--- js/script.js
+++ js/script.js
@@ -2,16 +2,17 @@ for (var i = 1; i <= 100; i++) {
     parola= '';
     if ((i % 3 == 0) && (i % 5 == 0)) {
         parola = 'FizzBuzz';
-        console.log(parola)
-        i++;
+        document.getElementById('lista').innerHTML += '<li>' + parola + '</i>';
+
     } else if (i % 5 == 0) {
         parola = 'Buzz';
-        console.log(parola)
-        i++;
+        document.getElementById('lista').innerHTML += '<li>' + parola + '</i>';
+
     } else if(i % 3 == 0){
         parola = 'Fizz';
-        console.log(parola)
-        i++;
+        document.getElementById('lista').innerHTML += '<li>' + parola + '</i>';
+
+    }  else {
+        document.getElementById('lista').innerHTML += '<li>' + i + '</i>';
     }
-    console.log(i);
 }
