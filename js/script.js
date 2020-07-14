1a46beb6cdccc07d69efdb3ed82d2fdb5864115d
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-07-14 14:01:21 +0200

    Complete

--- js/script.js
+++ js/script.js
@@ -3,15 +3,12 @@ for (var i = 1; i <= 100; i++) {
     if ((i % 3 == 0) && (i % 5 == 0)) {
         parola = 'FizzBuzz';
         document.getElementById('lista').innerHTML += '<li>' + parola + '</i>';
-
     } else if (i % 5 == 0) {
         parola = 'Buzz';
         document.getElementById('lista').innerHTML += '<li>' + parola + '</i>';
-
     } else if(i % 3 == 0){
         parola = 'Fizz';
         document.getElementById('lista').innerHTML += '<li>' + parola + '</i>';
-
     }  else {
         document.getElementById('lista').innerHTML += '<li>' + i + '</i>';
     }
