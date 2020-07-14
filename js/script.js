2b842be91e4e781c6a39fdc54bbf6b0cd877b71f
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-07-14 13:06:25 +0200

    Checkpoint n.2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 for (var i = 1; i <= 100; i++) {
     parola= '';
-    console.log(i);
     if ((i % 3 == 0) && (i % 5 == 0)) {
         parola = 'FizzBuzz';
-        console.log(i + ' Ha il nome di: ' + parola);
+        console.log(parola)
+        i++;
     } else if (i % 5 == 0) {
         parola = 'Buzz';
-        console.log(i + ' Ha il nome di: ' + parola);
+        console.log(parola)
+        i++;
     } else if(i % 3 == 0){
         parola = 'Fizz';
-        console.log(i + ' Ha il nome di: ' + parola);
+        console.log(parola)
+        i++;
     }
+    console.log(i);
 }
