b8f80db6598b350a2e4f019091664f8d3dfc534e
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-07-14 14:27:01 +0200

    alternative

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
+// FIRST ALTERNATIVE
+
+// for (var i = 1; i <= 100; i++) {
+//     parola= '';
+//     if ((i % 3 == 0) && (i % 5 == 0)) {
+//         parola = 'FizzBuzz';
+//         document.getElementById('lista').innerHTML += '<li>' + parola + '</i>';
+//     } else if (i % 5 == 0) {
+//         parola = 'Buzz';
+//         document.getElementById('lista').innerHTML += '<li>' + parola + '</i>';
+//     } else if(i % 3 == 0){
+//         parola = 'Fizz';
+//         document.getElementById('lista').innerHTML += '<li>' + parola + '</i>';
+//     }  else {
+//         document.getElementById('lista').innerHTML += '<li>' + i + '</i>';
+//     }
+// }
+
+// SECOND ALTERNATIVE
+
 for (var i = 1; i <= 100; i++) {
     parola= '';
     if ((i % 3 == 0) && (i % 5 == 0)) {
         parola = 'FizzBuzz';
-        document.getElementById('lista').innerHTML += '<li>' + parola + '</i>';
     } else if (i % 5 == 0) {
         parola = 'Buzz';
-        document.getElementById('lista').innerHTML += '<li>' + parola + '</i>';
     } else if(i % 3 == 0){
         parola = 'Fizz';
+    }
+    if (parola) {
         document.getElementById('lista').innerHTML += '<li>' + parola + '</i>';
     }  else {
         document.getElementById('lista').innerHTML += '<li>' + i + '</i>';
